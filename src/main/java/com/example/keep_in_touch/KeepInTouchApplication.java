713239075764ad67e32765f688c41f2824c8bdb6
9elmaz9 <@.com>
2024-08-08 15:50:01 +0200

    Initial commit

---
+++ src/main/java/com/example/keep_in_touch/KeepInTouchApplication.java
@@ -0,0 +1,13 @@
+package com.example.keep_in_touch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeepInTouchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeepInTouchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/keep_in_touch/KeepInTouchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.keep_in_touch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeepInTouchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
