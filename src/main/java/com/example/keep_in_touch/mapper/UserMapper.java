98a1f3fc3c049f7da1c500c07d54ad84c9afcaa1
9elmaz9 <@.com>
2024-08-12 21:20:55 +0200

    userMap

--- src/main/java/com/example/keep_in_touch/mapper/UserMapper.java
+++ src/main/java/com/example/keep_in_touch/mapper/UserMapper.java
@@ -1,12 +1,35 @@
 package com.example.keep_in_touch.mapper;
 
 
+import com.example.keep_in_touch.dto.UserDTO;
+import com.example.keep_in_touch.entities.User;
 import org.springframework.stereotype.Component;
 
 /** Mapper class for converting between User entities and UserDTO objects.
  */
-@Component
+@Component // будет управляться контейнером Spring.
 public class UserMapper {
+
+    // Converts a User entity to a UserDTO object
+    //Преобразует сущность User в объект UserDTO,не раскрывая внутреннюю структуру entity.
+    public UserDTO toDto(User user){
+        UserDTO dto = new UserDTO();
+        // Call method to map common fields/Вызов метода для сопоставления общих полей
+        mapCommonFields(user,dto);
+        // Set the value to validated, check for null/ Установка значения validated, проверка на null - чтобы избежать возможных ошибок при преобразовании данных.
+        dto.setValidated(user.getValidated() != null ? user.getValidated() : 0);
+    return dto;
+    }
+
+
+    //  Converts a UserDTO object to a User entity/Преобразует объект UserDTO в сущность User
+    public User toEntity(UserDTO dto){
+
+    }
+
+    private void mapCommonFields(User user, UserDTO dto){}
+
+    private void mapCommonField(UserDTO dto, User user){}
 
 
 }
