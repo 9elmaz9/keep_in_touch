0853a93095bce2bade67cad9a419e04c11a7e328
9elmaz9 <@.com>
2024-08-20 01:06:05 +0200

    errorcontroller

--- src/main/java/com/example/keep_in_touch/controller/CustomErrorController.java
+++ src/main/java/com/example/keep_in_touch/controller/CustomErrorController.java
@@ -1,4 +1,18 @@
 package com.example.keep_in_touch.controller;
 
-public class CustomErrorController {
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+//This controller is responsible for displaying a custom error page when errors occur in the application.
+@Controller    //the ErrorController interface, which provides methods to handle error paths
+public class CustomErrorController implements ErrorController {
+
+    //This method is mapped to the "/error" endpoint and is triggered when an error occurs.
+    //It returns the name of the error view, which will be rendered to the user.
+    @GetMapping("/error")
+    public String handleError(){
+        //Return the view name "error", which Spring will use to render the error page.
+        return "error";
+    }
+
 }
