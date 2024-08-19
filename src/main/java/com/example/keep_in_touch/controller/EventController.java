3902c703d76f583161787c07191dee3cdb80864f
9elmaz9 <@.com>
2024-08-20 00:11:52 +0200

    eventcontroller

--- src/main/java/com/example/keep_in_touch/controller/EventController.java
+++ src/main/java/com/example/keep_in_touch/controller/EventController.java
@@ -33,14 +33,13 @@ public class EventController {
     private EventMapper eventMapper;
 
 
-
     //Метод addEvent для добавления события
     @PostMapping("/add")
     public String addEvent(@RequestBody EventDTO eventDTO, // dto содержащий данные нового события
                            @RequestParam Long contactId,  //Идентификатор контакта, связанного с этим событием
-                           Principal principal){  ////Проверяется, аутентифицирован ли пользователь
+                           Principal principal) {  ////Проверяется, аутентифицирован ли пользователь
 
-        if(principal == null){
+        if (principal == null) {
             return "Unauthorized";
         }
         User user = userService.getUserByUsername(principal.getName()); //Получаем текущего пользователя по имени из объекта Principal
@@ -51,19 +50,17 @@ public class EventController {
         event.setContact(contact);
 
         eventService.saveEvent(event); //сохраняем событие в базе данных
-        return  "Event added successfully "; // //Возвращаемое значение: Метод возвращает строку с сообщением о результате операции.
+        return "Event added successfully "; // //Возвращаемое значение: Метод возвращает строку с сообщением о результате операции.
     }
-
-
 
 
     @GetMapping("/user")
     //Метод getUserEvents для получения событий пользователя
     //Проверка, аутентифицирован ли пользователь.
     //Получение событий, связанных с пользователем, преобразование их в DTO и возврат списка событий
-    public List<EventDTO> getUserEvents(Principal principal){
-        if ( principal == null){
-            return  null;
+    public List<EventDTO> getUserEvents(Principal principal) {
+        if (principal == null) {
+            return null;
         }
         User user = userService.getUserByUsername(principal.getName());
         return eventService.findEventsByUserId(user.getId()).stream()
@@ -76,23 +73,75 @@ public class EventController {
     }
 
 
-    @GetMapping("/show-events")
-    public String showEvents(Model model, Principal principal){
-        return  null;
+    @GetMapping("/show-events")  //GET-запросы по пути /api/events/show-events
+    public String showEvents(Model model, Principal principal) {
+        //Если пользователь не аутентифицирован, происходит редирект на страницу входа.
+        //События пользователя извлекаются и добавляются в модель для отображения на странице show_events
+
+        if ( principal == null){
+            return  "redirect:/login"; // если не авториз покругу  редирект к логину
+        }
+        User user = userService.getUserByUsername(principal.getName());
+        List<EventDTO> events = eventService.findEventsByUserId(user.getId()).stream()
+                .map(event -> {
+                    EventDTO dto = eventMapper.toDTO(event);
+                    dto.setContactName(event.getContact().getName());
+                    return dto;
+                })
+                .collect(Collectors.toList());
+        model.addAttribute("events",events);
+        return "show_events"; //возвращает значение- имя представления шаблона , которое будет отображаться
     }
 
+
+    //Метод обрабатывает PUT-запросы по пути /api/events/update/{id}, где {id} — идентификатор события
     @PutMapping("/update/{id}")
+    //Метод updateEvent для обновления события
+    //Логика метода:
+    // Проверка аутентификации.
+    // Проверка, принадлежит ли событие текущему пользователю.
+    // Обновление информации о событии и сохранение изменений.
     public String updateEvent(@PathVariable Long id,
                               @RequestBody EventDTO eventDetails,
                               @RequestParam Long contactId,
-                              Principal principal){
-        return null;
+                              Principal principal) {
+        if ( principal ==null){
+            return "Unauthorized";
+        }
+        Event event = eventService.findEventById(id);
+        if ( event == null || !event.getUser().getEmail().equals(principal.getName())){
+            return "Event not found or you do not have permission to update this event";
+        }
+
+        Contact contact = contactService.getContactById(contactId.intValue());
+
+        event.setTitle(eventDetails.getTitle());
+        event.setStart(eventDetails.getStart());
+        event.setContact(contact);
+        event.setNote(eventDetails.getNote());
+
+        eventService.saveEvent(event);
+        return "Event updated successfully!";  //возвращает значение- сообщение  о результате
     }
 
 
     @DeleteMapping("/delete/{id}")
-    public String deleteEvent(@PathVariable Long id, Principal principal){
-        return null;
+    // Метод deleteEvent для удаления события
+    //Логика метода:
+    //Проверка аутентификации.
+    //Проверка, принадлежит ли событие текущему пользователю.
+    //Удаление события из базы данных.
+    public String deleteEvent(@PathVariable Long id, Principal principal) {
+
+        if(principal == null){
+            return "Unauthorized";
+        }
+        Event event = eventService.findEventById(id);
+        if ( event == null || !event.getUser().getEmail().equals(principal.getName())){
+            return "Event not found or you do not have permission to update this event";
+        }
+        eventService.deleteEvent(id);
+        return "Event deleted successfully!"; //возвращает значение - сообщение о результате операции
 
     }
 }
